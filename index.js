ed6d1794eb47147bfe18e5873d1d55d68e67b6f7
Miguel M-Buitrago <miguelmbfp@gmail.com>
2022-01-13 18:26:50 +0100

    routes methods refactoring

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const Note = require('./models/Note')
 const notFound = require('./middleware/notFound')
 const handleErrors = require('./middleware/handleErrors')
+
 app.use(cors())
 app.use(express.json())
 
@@ -14,24 +15,25 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello world</h1>')
 })
 
-app.get('/api/notes', (request, response, next) => {
-  Note.find({})
-    .then(notes => response.json(notes))
-    .catch(next)
+app.get('/api/notes', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response, next) => {
+app.get('/api/notes/:id', async (request, response, next) => {
   const id = request.params.id
 
-  Note.findById(id)
-    .then(note => {
-      if (note) response.json(note)
-      response.status(404).end()
-    })
-    .catch(next)
+  try {
+    const note = await Note.findById(id)
+    note
+      ? response.json(note)
+      : response.status(404).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.put('/api/notes/:id', async (request, response, next) => {
   const { id } = request.params
   const note = request.body
 
@@ -40,20 +42,26 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: note.important
   }
 
-  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-    .then(result => response.json(result))
-    .catch(next)
+  try {
+    const newNote = await Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+    response.json(newNote)
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id
+app.delete('/api/notes/:id', async (request, response, next) => {
+  const { id } = request.params
 
-  Note.findByIdAndDelete(id)
-    .then(() => response.status(204).end())
-    .catch(next)
+  try {
+    await Note.findByIdAndDelete(id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.post('/api/notes', (request, response, next) => {
+app.post('/api/notes', async (request, response, next) => {
   const note = request.body
 
   if (!note || !note.content) {
@@ -68,9 +76,12 @@ app.post('/api/notes', (request, response, next) => {
     important: note.important || false
   })
 
-  newNote.save()
-    .then(savedNote => response.json(savedNote))
-    .catch(next)
+  try {
+    const savedNote = await newNote.save()
+    response.json(savedNote)
+  } catch (error) {
+    next(error)
+  }
 })
 
 app.use(notFound)
@@ -79,6 +90,8 @@ app.use(handleErrors)
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = { app, server }
